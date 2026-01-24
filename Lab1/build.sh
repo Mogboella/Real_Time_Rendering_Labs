715cedb655bf0aa2be55e3884e3e3e4bafaa7cee
noella2001 <mogboella@gmail.com>
2026-01-24 07:53:15 +0000

    fix build

--- Lab1/build.sh
+++ Lab1/build.sh
@@ -7,7 +7,7 @@
 rm -rf build
 
 # Create fresh build directory and configure
-mkdir build && cd build
+mkdir build && cd build || return 1
 cmake .. -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_OSX_SYSROOT="$(xcrun --show-sdk-path)"
 make -j
